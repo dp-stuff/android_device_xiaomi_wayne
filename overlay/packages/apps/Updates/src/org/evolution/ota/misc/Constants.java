b10b68b2c9d254a8b2226e09a4ec51ac6e04f384
OdSazib <odsazib@gmail.com>
2020-05-27 08:08:42 +0600

    OTA: overlay for updater apps to track custom url Thanks @shoukolate for helping with this

---
+++ overlay/packages/apps/Updates/src/org/evolution/ota/misc/Constants.java
@@ -0,0 +1,2 @@
+static final String OTA_URL = "https://raw.githubusercontent.com/OdSazib/unofficial_ota/ten/builds/%s.json";
+static final String DOWNLOAD_WEBPAGE_URL = "https://raw.githubusercontent.com/OdSazib/unofficial_ota/ten/changelogs/%s/%s.txt";
